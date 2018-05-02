7cee8e6774729d04c60a6899b73fa96348f46cb8
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2018-05-02 14:00:00 -0400

    one step qlearning done?

--- src/utils.py
+++ src/utils.py
@@ -7,3 +7,19 @@ if torch.cuda.is_available():
 else:
     FloatTensor = torch.FloatTensor
 
+GAMES = {'pong': 'PongNoFrameskip-v4',
+         'seaquest': 'SeaquestNoFrameskip-v4',
+         'spaceinvaders': 'SpaceInvadersNoFrameskip-v4',
+         'frostbite': 'FrostbiteNoFrameskip-v4',
+         'beamrider': 'BeamRiderNoFrameskip-v4'}
+
+class Epsilon(object):
+
+    def __init__(self, start, end, decay):
+        self.start = start
+        self.end = end
+        self.decay = decay
+
+    def __call__(self, t):
+        return self.end + max(0, 1 - t / self.decay) * (self.start - self.end)
+
